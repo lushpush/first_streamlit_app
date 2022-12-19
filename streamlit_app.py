4f65287df6f115ee12fd82f1e668c08f2bee9939
Farhan Shaikh <farhan.shaikh@vizientinc.com>
2022-12-19 16:42:54 -0600

    Added first check

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit 
+import pandas
+
 streamlit.title("My Parents new healthy Diner!")
 streamlit.header("Breakfast Menu")
 streamlit.text ("🥣 Omega 3 & Blueberry Oatmeal")
@@ -7,3 +9,5 @@ streamlit.text ("🐔Hard-Boiled Free-Range Egg")
 streamlit.text ("🥑🍞 Avacardo Toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list  = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
