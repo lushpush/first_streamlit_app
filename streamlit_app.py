835d8b072bea337b8be0178ba5009818b92995fc
Farhan Shaikh <farhan.shaikh@vizientinc.com>
2022-12-19 16:34:44 -0600

    Added first check

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit 
 streamlit.title("My Parents new healthy Diner!")
+streamlit.header("Breakfast Menu")
+streamlit.text ("Omega 3 & Blueberry Oatmeal")
+streamlit.text ("kale, Spinach & Rocket smoothieOatmeal")
+streamlit.text ("Hard-Boiled Free-Range Egg")
+
