0687bc51bed14304968eee53523d6bd1abeb98db
Farhan Shaikh <farhan.shaikh@vizientinc.com>
2022-12-22 17:21:14 -0600

    Added first check

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.sidebar.markdown("first page")
 streamlit.sidebar.markdown("second page")
 streamlit.sidebar.markdown("Third page")
+
+streamlit.header("Fruitvice fruit Advice")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
