4ab266a26e97e94bc437cfe23cabcf014e5cd223
Farhan Shaikh <farhan.shaikh@vizientinc.com>
2022-12-22 17:24:04 -0600

    Added first check

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ streamlit.sidebar.markdown("second page")
 streamlit.sidebar.markdown("Third page")
 
 streamlit.header("Fruitvice fruit Advice")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
