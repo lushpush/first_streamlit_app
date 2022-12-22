41db6c76bdbae59404bfcda6d228516d752b9c79
Farhan Shaikh <farhan.shaikh@vizientinc.com>
2022-12-22 17:26:23 -0600

    Added first check

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,10 @@ streamlit.sidebar.markdown("second page")
 streamlit.sidebar.markdown("Third page")
 
 streamlit.header("Fruitvice fruit Advice")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
