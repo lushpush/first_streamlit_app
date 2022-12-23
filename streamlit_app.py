7698b7aef78f38d2329efb01bf7f87592e99f919
Farhan Shaikh <farhan.shaikh@vizientinc.com>
2022-12-22 18:22:04 -0600

    Added first check

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,11 +48,10 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT  * from fruit_load_list")
 my_data_row = my_cur.fetchall()
-
+streamlit.dataframe(my_data_row)
 
 fruit_choice2 = streamlit.text_input('What fruit you like to add','jackfruit')
 streamlit.write('Thanks for adding ', fruit_choice2)
-streamlit.dataframe(my_data_row)
 
 
 
